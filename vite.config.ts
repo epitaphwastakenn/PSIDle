1d13e82afc2f4986408202a3e975d77336577e18
epitaphwastaken <thesupremespauder@gmail.com>
2026-05-29 05:45:32 -0300

    Fixing white page on Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubActions && repositoryName ? `/${repositoryName}/` : '/',
+  base: './',
 })
